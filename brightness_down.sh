5d80b41f497d7f28b53b7cf3c10e720ddfc395b7
Balázs Orbán <info@balazsorban.com>
2018-02-11 14:00:22 +0100

    :robot: Lazy commit by a robot.

--- brightness_down.sh
+++ brightness_down.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Find screen name
+SCREEN=`xrandr | grep " connected" | awk '{print $1;}'`
+
+# Find current brightness
 BRIGHTNESS=`stdbuf -o0 xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' '`
+
+# Remove .1 from it
 BRIGHTNESS=`awk '{print $1-$2}' <<<"$BRIGHTNESS .1"`
-xrandr --output eDP-1 --brightness  $BRIGHTNESS
+
+# Set new brightness
+xrandr --output $SCREEN --brightness  $BRIGHTNESS
