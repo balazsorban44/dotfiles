eeeff513b6dda0144668d7c39fe1d12ebd2938e8
Balázs Orbán <info@balazsorban.com>
2018-02-11 11:52:09 +0100

    :heavy_plus_sign: Add dotfiles by rhysd
    https://github.com/rhysd/dotfiles

---
+++ brightness_down.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+BRIGHTNESS=`stdbuf -o0 xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' '`
+BRIGHTNESS=`awk '{print $1-$2}' <<<"$BRIGHTNESS .1"`
+xrandr --output eDP-1 --brightness  $BRIGHTNESS
